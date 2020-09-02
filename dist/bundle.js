300053e1b2c228d396f237a5931ae07dc5287061
dancreppel <creppel@usc.edu>
2020-09-01 17:13:57 -0700

    Commit All

--- dist/bundle.js
+++ dist/bundle.js
@@ -86,17 +86,80 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/entity.js":
+/*!***********************!*\
+  !*** ./src/entity.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Entity; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Entity = /*#__PURE__*/function () {
+  function Entity(options) {
+    _classCallCheck(this, Entity);
+
+    // pos is an array with x and y coordinates [x, y]
+    this.pos = options.pos; // dim is an array with width and height [width, height]
+
+    this.dim = options.dim;
+    this.image = new Image();
+    this.image.src = options.src;
+  }
+
+  _createClass(Entity, [{
+    key: "draw",
+    value: function draw(ctx) {
+      ctx.drawImage(this.image, this.pos[0], this.pos[1], this.dim[0], this.dim[1]);
+    }
+  }, {
+    key: "move",
+    value: function move(dx, dy) {
+      this.pos[0] += dx;
+      this.pos[1] += dy;
+    }
+  }]);
+
+  return Entity;
+}();
+
+
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _entity__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./entity */ "./src/entity.js");
 
 document.addEventListener("DOMContentLoaded", function () {
-  var element = document.createElement('h1');
-  element.innerHTML = "Hello World";
-  document.body.appendChild(element);
+  var canvas = document.getElementById('canvas');
+  var DIM_X = 1200;
+  var DIM_Y = 900;
+  canvas.width = DIM_X;
+  canvas.height = DIM_Y;
+  window.ctx = canvas.getContext('2d');
+  var rock = new _entity__WEBPACK_IMPORTED_MODULE_0__["default"]({
+    pos: [550, 500],
+    dim: [200, 150],
+    src: 'assets/sprites/rock.jpg'
+  });
+  window.rock = rock; // const img = new Image();
+  // img.onload = () => ctx.drawImage(img, 50, 50);
+  // img.src = 'assets/sprites/rock.jpg';
 });
 
 /***/ })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import Entity from './entity';
+
 document.addEventListener("DOMContentLoaded", () => {
-  const element = document.createElement('h1');
-  element.innerHTML = "Hello World"
-  document.body.appendChild(element);
+  const canvas = document.getElementById('canvas');
+  const DIM_X = 1200;
+  const DIM_Y = 900;
+  canvas.width = DIM_X;
+  canvas.height = DIM_Y;
+  window.ctx = canvas.getContext('2d');
+  let rock = new Entity({ 
+    pos: [550, 500], 
+    dim: [200, 150],
+    src: 'assets/sprites/rock.jpg' });
+  window.rock = rock;
+
+  // const img = new Image();
+  // img.onload = () => ctx.drawImage(img, 50, 50);
+  // img.src = 'assets/sprites/rock.jpg';
 });
