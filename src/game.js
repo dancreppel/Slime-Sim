7eb22625d66555a18d424264cbdbdc12010465a7
dancreppel <creppel@usc.edu>
2020-09-03 17:42:06 -0700

    Add keybinds for arrow keys

--- src/game.js
+++ src/game.js
@@ -7,6 +7,8 @@ export default class Game {
     this.DIM_Y = options.DIM_Y;
     this.ctx = options.ctx;
     this.entities = [];
+    this.moveDirX = 0;
+    this.moveDirY = 0;
 
     this.createPlayer();
     this.generateEntities();
@@ -31,7 +33,7 @@ export default class Game {
   generateEntities () {
     // * For testing
     this.rock = new Entity ({
-      pos: [550, 500],
+      pos: [0, 0],
       dim: [200, 150],
       src: 'assets/sprites/rock.jpg'
     });
@@ -47,9 +49,51 @@ export default class Game {
   start () {
     // refresh 60 times per second
     setInterval(() => this.render(), 16.667)
+    this.setKeyBinds();
+  }
+
+  setKeyBinds () {
+    // handle keydownfor arrow keys
+    document.addEventListener('keydown', e => {
+      e.preventDefault();
+      switch (e.key) {
+        case ArrowUp:
+          this.moveDirY = 1;
+          break;
+        case ArrowDown:
+          this.moveDirY = -1;
+          break;
+        case ArrowLeft:
+          this.moveDirX = -1;
+          break;
+        case ArrowRight:
+          this.moveDirX = 1;
+          break;
+        default:
+          break;
+      }
+    });
+
+    // handle keyup for arrow keys
+    document.addEventListener('keyup', e => {
+      e.preventDefault();
+      const horKeys = ['ArrowLeft', 'ArrowRight'];
+      const verKeys = ['ArrowUp', 'ArrowDown']; 
+      debugger;
+      if (horKeys.includes(e.key)) {
+        this.moveDirY = 0;
+      }
+
+      if (verKeys.includes(e.key)) {
+        this.moveDirY = 0;
+      }
+    })
   }
 
   moveEntities () {
-    this.entities.forEach
+    // * testing
+    this.entities.forEach(entity => {
+      entity.move(this.moveDirX, this.moveDirY);
+    })
   }
 }
