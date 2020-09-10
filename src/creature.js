0672ba6201fd95b9b88dc655659b2568d4dd5bdb
dancreppel <creppel@usc.edu>
2020-09-09 18:32:39 -0700

    Add logic for collisions

--- src/creature.js
+++ src/creature.js
@@ -3,6 +3,11 @@ import Entity from './entity';
 export default class Creature extends Entity {
   constructor (options) {
     super(options);
+    // this.pos
+    // this.dim
+    // this.image
+    // this.hitboxCenter
+    // this.hitboxRadius
   }
 
   move() {
@@ -10,6 +15,16 @@ export default class Creature extends Entity {
     // reference
 
     // ! hit box for testing
-    this.hitbox();
+    this.drawHitbox();
+  }
+
+  isCollision (entity) {
+    let dx = this.hitboxCenter[0] - entity.hitboxCenter[0];
+    let dy = this.hitboxCenter[1] - entity.hitboxCenter[1];
+    let distance = Math.sqrt( dx * dx + dy * dy );
+    let minDistance = this.hitboxRadius + entity.hitboxRadius;
+
+    if (distance < minDistance) return true;
+    else return false;
   }
 }
