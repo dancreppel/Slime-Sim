4dfb486eb5c564d0e38d21c0b99cab788f41f366
dancreppel <creppel@usc.edu>
2020-09-09 18:33:13 -0700

    make slime position absolute

--- src/slime.js
+++ src/slime.js
@@ -5,5 +5,11 @@ export default class Slime extends Creature {
     super(options);
   }
   
+  move() {
+    // Do not move because the player should be in the center of the frame of
+    // reference
 
+    // ! hit box for testing
+    this.drawHitbox();
+  }
 }
