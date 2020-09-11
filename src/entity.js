a65767b9e58e914b1cb1d09f763e04b65e9b5028
dancreppel <creppel@usc.edu>
2020-09-11 15:39:50 -0700

    Create working collision

--- src/entity.js
+++ src/entity.js
@@ -1,5 +1,5 @@
 export default class Entity {
-  constructor (options) {
+  constructor(options) {
     // pos is an array with x and y coordinates [x, y]
     this.pos = options.pos;
     // dim is an array with width and height [width, height]
@@ -10,54 +10,70 @@ export default class Entity {
     // Instantiate hitbox
     this.hitboxCenter = this.hitboxCenter();
     this.hitboxRadius = this.hitboxRadius();
-    // this.hitbox();
   }
 
-  draw (ctx) {
+  draw(ctx) {
     ctx.drawImage(
-      this.image, 
-      this.pos[0], 
-      this.pos[1], 
-      this.dim[0], 
-      this.dim[1]);
+      this.image,
+      this.pos[0],
+      this.pos[1],
+      this.dim[0],
+      this.dim[1]
+    );
+
+    // // ! for testing
+    // this.drawHitbox();
   }
 
-  move (dx, dy) {
+  move(dx, dy, ...entities) {
     this.pos[0] += dx;
     this.pos[1] += dy;
     this.hitboxCenter[0] += dx;
     this.hitboxCenter[1] += dy;
 
-    // ! for testing
-    this.drawHitbox();
+    if (this.isCollision(entities[0])) {
+      // undo move with a little bounce back
+      this.pos[0] -= 1.1 * dx;
+      this.pos[1] -= 1.1 * dy;
+      this.hitboxCenter[0] -= 1.1 * dx;
+      this.hitboxCenter[1] -= 1.1 * dy;
+    }
   }
 
-  hitboxCenter () {
-    let hitboxCenter = this.pos.map((_, i) => this.pos[i] + this.dim[i] / 2 );
+  hitboxCenter() {
+    let hitboxCenter = this.pos.map((_, i) => this.pos[i] + this.dim[i] / 2);
     return hitboxCenter;
   }
 
-  hitboxRadius () {
-    return this.dim[0] > this.dim[1] ? this.dim[0] / 2 : this.dim[1] / 2;
+  hitboxRadius() {
+    return this.dim[0] < this.dim[1] ? this.dim[0] / 2 : this.dim[1] / 2;
   }
 
-  // ! for testing
-  drawHitbox () {
-    // this.hitboxCenter = [];
-    // this.pos.forEach((_, i) =>
-    //   this.hitboxCenter.push(this.pos[i] + this.dim[i] / 2)
-    // );
-    // // this.hitboxCenter = [this.pos[0] + this.dim[0] / 2, this.pos[1] + this.dim[1] / 2];
-    // // if (this.dim[0] > this.dim[1]) this.hitboxRadius = this.dim[0] / 2;
-    // this.hitboxRadius = this.dim[0] > this.dim[1] ? this.dim[0] / 2 : this.dim[1] / 2;
+  // // ! for testing
+  // drawHitbox() {
+  //   const canvas = document.getElementById("canvas");
+  //   const ctx = canvas.getContext("2d");
+  //   ctx.beginPath();
+  //   ctx.arc(
+  //     this.hitboxCenter[0],
+  //     this.hitboxCenter[1],
+  //     this.hitboxRadius,
+  //     0,
+  //     2 * Math.PI,
+  //     false
+  //   );
+  //   ctx.stroke();
 
-    // // ! for testing
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.arc(this.hitboxCenter[0], this.hitboxCenter[1], this.hitboxRadius, 0, 2 * Math.PI, false);
-    ctx.stroke();
+  //   return true;
+  // }
 
-    return true;
+  isCollision(entity) {
+    let dx = this.hitboxCenter[0] - entity.hitboxCenter[0];
+    let dy = this.hitboxCenter[1] - entity.hitboxCenter[1];
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let minDistance = this.hitboxRadius + entity.hitboxRadius;
+
+    if (distance < minDistance) return true;
+    else return false;
   }
 }
