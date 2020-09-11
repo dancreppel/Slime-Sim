9c55b407c3deeed9505513d2ce99e82e861e998d
dancreppel <creppel@usc.edu>
2020-09-11 15:40:41 -0700

    Move collision logic to parent (entity)

--- src/creature.js
+++ src/creature.js
@@ -14,17 +14,7 @@ export default class Creature extends Entity {
     // Do not move because the player should be in the center of the frame of
     // reference
 
-    // ! hit box for testing
-    this.drawHitbox();
-  }
-
-  isCollision (entity) {
-    let dx = this.hitboxCenter[0] - entity.hitboxCenter[0];
-    let dy = this.hitboxCenter[1] - entity.hitboxCenter[1];
-    let distance = Math.sqrt( dx * dx + dy * dy );
-    let minDistance = this.hitboxRadius + entity.hitboxRadius;
-
-    if (distance < minDistance) return true;
-    else return false;
+    // // ! hit box for testing
+    // this.drawHitbox();
   }
 }
