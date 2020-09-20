733b00a67c99ffde6550fa2029ed7b67a8ef21a4
dancreppel <creppel@usc.edu>
2020-09-19 19:35:08 -0700

    Allow slime to eat enemies

--- src/slime.js
+++ src/slime.js
@@ -5,8 +5,24 @@ export default class Slime extends Creature {
     super(options);
   }
   
-  move() {
+  move () {
     // Do not move because the player should be in the center of the frame of
     // reference
+  }
+
+  eat (enemies) {
+    enemies.forEach(enemy => {
+      if (this.eatable(enemy)) delete enemy;
+    });
+  }
+
+  eatable (enemy) {
+    let dx = this.hitboxCenter[0] - entity.hitboxCenter[0];
+    let dy = this.hitboxCenter[1] - entity.hitboxCenter[1];
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let minDistance = this.hitboxRadius;
+
+    if (distance < minDistance) return true;
+    else return false;
   }
 }
