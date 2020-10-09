50e7784e46b9b983e63f71f29a0374b185f69256
dancreppel <creppel@usc.edu>
2020-10-08 19:59:40 -0700

    Let game view handle game instance

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-import Game from './game';
 import GameView from './game_view';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById('canvas');
-  const DIM_X = 1600;
-  const DIM_Y = 900;
-  const ctx = canvas.getContext('2d');
-  const movementSpeed = 10;
-
-  canvas.width = DIM_X;
-  canvas.height = DIM_Y;
-  
-  let game = new Game({
-    DIM_X,
-    DIM_Y,
-    ctx,
-    movementSpeed,
-    ambientSrc: "assets/sounds/ambient.wav"
-  });
-  
-  game.start();
-
-  let gameView = new GameView({
-    game
-  })
+  new GameView();
 });
